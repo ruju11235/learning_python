8b979f6da02d3c619e8225903e9fcecd807745fd
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-12 15:02:01 +0530

    Added some more code to the input output file

--- input_output.py
+++ input_output.py
@@ -1,2 +1,13 @@
 name = input("What is your name? ")
 print("Hello,", name)
+
+age = input("How old are you? ")
+print("Have fun being", age)
+
+favc = input("What is your favorite color? ")
+print("My favorite color is green!")
+
+favf = input("What is your favorite food? ")
+print("My favorite food is ice cream! I can eat however full I am!")
+
+print("Bye!")
