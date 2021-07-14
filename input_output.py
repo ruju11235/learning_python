74e65451288fb63ac7841156c1d9b622c9a3b0dd
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-14 15:51:47 +0530

    Learned about different data types

--- input_output.py
+++ input_output.py
@@ -1,8 +1,13 @@
 name = input("What is your name? ")
 print("Hello,", name)
 
-age = input("How old are you? ")
-print("Have fun being", age)
+age = int(input("How old are you? "))
+while age < 0:
+    print("Why are you being mean to my program? Enter a valid value!!!")
+    age = int(input("How old are you? "))
+else:
+    print("Have fun being", age)
+    print("Next year you will be", age+1)
 
 favc = input("What is your favorite color? ")
 print("My favorite color is green!")
---
+++ learn_data_types.py
@@ -0,0 +1,24 @@
+# Learning python data types.
+
+PI = 3.14
+
+# Playing with numbers!
+
+dark = 4
+print("Currently there are", dark, "people in my family.")
+print("When my Baba Aaji comes, there will be", dark+1, "people in my house.")
+print("When my Aai Aaji and Aai Aaba come, there will be", dark+2, "people in my house.")
+
+sasa = 4
+print(sasa, "is the number of people in my Mama's family.")
+print("Number of family members living in Sigma is", dark+sasa)
+
+print(dark)
+dark += 1
+print(dark)
+
+print("a:", sasa)
+print("b:", sasa+10)
+print("c:", sasa)
+sasa = sasa+10
+print("d:", sasa)
