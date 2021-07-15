5d6a68c0741fc2ad782dae7825b08645d6648411
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-15 15:35:07 +0530

    Learned more about numbers

---
+++ learn_numbers.py
@@ -0,0 +1,30 @@
+# Learning what can be done with numbers in Python!
+
+a = 10
+b = 5
+c = 3
+print('a, b, c =', a, b, c)
+print('a+b =', a + b)
+print("a-b =", a - b)
+print(a * b)
+print(a / b)
+print(a / c)
+
+print(a // b)
+print(a // c)
+
+print(divmod(a, b))
+print(divmod(a, c))
+
+print(a % b)
+print(a % c)
+
+print(a ** b)
+print(a ** c)
+
+print()
+x = 2.5
+y = 4
+print(x * y)
+print(divmod(y, x))
+print(divmod(x, y))
