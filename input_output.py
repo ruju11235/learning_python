88308c39555179240dd75ec29bf451a78e363edb
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-16 14:42:49 +0530

    Added some minor changes

--- input_output.py
+++ input_output.py
@@ -1,6 +1,7 @@
 name = input("What is your name? ")
 print("Hello,", name, "!")
 
+print()
 age = int(input("How old are you? "))
 while age < 0:
     print("Why are you being mean to my program? Enter a valid value!!!")
@@ -9,13 +10,18 @@ else:
     print("Have fun being", age, "!!!")
     print("Next year you will be", age + 1, ":)")
 
+print()
 favc = input("What is your favorite color? ")
 if favc == "green":
     print("My favorite color is also green!")
 else:
     print("My favorite color is green!")
 
+print()
 favf = input("What is your favorite food? ")
-print("My favorite food is ice cream! I can eat however full I am!")
-
+if favf == "ice cream":
+    print("My favorite food is also ice cream!")
+else:
+    print("My favorite food is ice cream! I can eat however full I am!")
+print("")
 print("Bye!")
