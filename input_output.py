fddbdd461723e313068ceac33538da16c75cda55
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-16 15:36:05 +0530

    Learned about strings

--- input_output.py
+++ input_output.py
@@ -2,7 +2,12 @@ name = input("What is your name? ")
 print("Hello,", name, "!")
 
 print()
-age = int(input("How old are you? "))
+age_str = input("How old are you? ")
+while not age_str.isdigit():
+    print("Why are you being mean to my program? Enter a valid value!!!")
+    age_str = input("How old are you? ")
+
+age = int(age_str)
 while age < 0:
     print("Why are you being mean to my program? Enter a valid value!!!")
     age = int(input("How old are you? "))
@@ -23,5 +28,5 @@ if favf == "ice cream":
     print("My favorite food is also ice cream!")
 else:
     print("My favorite food is ice cream! I can eat however full I am!")
-print("")
+print()
 print("Bye!")
---
+++ learn_strings.py
@@ -0,0 +1,20 @@
+str = "Hi! My name is Rujuta."
+print(str)
+
+f1 = "ice "
+f2 = "cream"
+print(f1 + f2)
+
+# age = "ab12cd"
+age_str = input("Enter your age. ")
+print(age_str)
+if age_str.isdigit():
+    print("Age is valid.")
+else:
+    print("Age should be a number.")
+
+word = input("Enter a word. ")
+if word.isalpha():
+    print("The word is valid")
+else:
+    print("The word is not vaild.")
