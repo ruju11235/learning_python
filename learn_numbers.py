d0902c5d658f8e39a381374b0e8c6f876d0201a5
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-18 20:42:02 +0530

    Added more code about numbers

--- learn_numbers.py
+++ learn_numbers.py
@@ -3,28 +3,33 @@
 a = 10
 b = 5
 c = 3
-print('a, b, c =', a, b, c)
-print('a+b =', a + b)
-print("a-b =", a - b)
+print(a + b)
+print(a - b)
 print(a * b)
 print(a / b)
-print(a / c)
 
+print(a // a)
 print(a // b)
 print(a // c)
 
+print(divmod(a, a))
 print(divmod(a, b))
 print(divmod(a, c))
 
+print(a % a)
 print(a % b)
 print(a % c)
 
+print(a ** a)
 print(a ** b)
 print(a ** c)
 
 print()
-x = 2.5
-y = 4
+x = 10
+y = 2
+print(x + y)
+print(x - y)
 print(x * y)
-print(divmod(y, x))
+print(x / y)
 print(divmod(x, y))
+print(divmod(y, x))
