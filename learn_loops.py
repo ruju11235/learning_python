53ddeb25bcb1aea164429a62b22b945b4ff019f8
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-07-29 11:57:18 +0530

    Learned about loops in python

---
+++ learn_loops.py
@@ -0,0 +1,12 @@
+start = int(input("Give a valid starting value! "))
+end = int(input("Give a valid ending value! "))
+while start > end:
+    print("Stop being mean to my program >:(")
+    start = int(input("Give a valid starting value! "))
+    end = int(input("Give a valid ending value! "))
+
+n = start
+while n <= end:
+    print(n)
+    n = n + 1
+
