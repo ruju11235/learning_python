25db057373356da51e57918c25ee2b9a9e88bf13
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-08-02 11:42:46 +0530

    Learned about 'for' loops

--- learn_loops.py
+++ learn_loops.py
@@ -1,24 +1,40 @@
-start = int(input("Give a valid starting value! "))
-end = int(input("Give a valid ending value! "))
-while start > end:
-    print("Stop being mean to my program >:(")
-    start = int(input("Give a valid starting value! "))
-    end = int(input("Give a valid ending value! "))
-
-n = start
-while n <= end:
-    print(n)
-    n = n + 1
-print()
-
+# start = int(input("Give a valid starting value! "))
+# end = int(input("Give a valid ending value! "))
+# while start > end:
+#     print("Stop being mean to my program >:(")
+#     start = int(input("Give a valid starting value! "))
+#     end = int(input("Give a valid ending value! "))
+#
+# n = start
+# while n < end:
+#     print(n)
+#     n = n + 1
+# print()
+#
 # infinite loop
 # n = start
 # while n <= end:
 #     print(n)
 #     n = n - 1
+#
+# n = start
+# step = int(input("How much would you like to increment by? "))
+# while n < end:
+#     print(n)
+#     n = n + step
+# print()
+#
+# for i in range(start, end):
+#     print(i)
+# print()
+#
+# for i in range(start, end, step):
+#     print(i)
+# print()
 
-n = start
-step = int(input("How much would you like to increment by? "))
-while n <= end:
-    print(n)
-    n = n + step
+name = input("Please enter your name: ")
+for x in name:
+    if x in ("A", "E", "I", "O", "U", "a", "e", "i", "o", "u"):
+        print(x, "vowel")
+    else:
+        print(x, "consonant")
