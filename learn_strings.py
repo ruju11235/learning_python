f4553b53bc20b29407eb9188313fa9a3014973e5
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-08-12 11:37:32 +0530

    Learned about different functions to use on strings

--- learn_strings.py
+++ learn_strings.py
@@ -1,20 +1,51 @@
-str = "Hi! My name is Rujuta."
+str = "My Name Is Rujuta."
+print(str)
+# print(dir(str))
+print('str.startswith("a")', str.startswith("a"))
+print('str.endswith(".")', str.endswith("."))
+
+print('"Kiki".isalpha() = ', "Kiki".isalpha())
+print('"Kiki123".isalpha() = ', "Kiki123".isalpha())
+print('"Kiki123".isdigit() = ', "Kiki123".isdigit())
+print('"Kiki123".isalnum() = ', "Kiki123".isalnum())
+print('str.isalnum() =', str.isalnum())
+print('"12.3".isdigit() = ', "12.3".isdigit())
+print('"12.3".isnumeric() = ', "12.3".isnumeric())
+print('str.isspace() =', str.isspace())
+print('"  ".isspace() =', "  ".isspace())
+
+print()
+
+print('"Kiki".islower() = ', "Kiki".islower())
+print('"kiki".islower() = ', "kiki".islower())
+print('"Kiki".isupper() = ', "Kiki".isupper())
+print('"KIKI".isupper() = ', "KIKI".isupper())
+print('str.istitle() = ', str.istitle())
+print()
+
+str = "hello world"
+print(str.capitalize())
+print(str.title())
+print(str.upper())
 print(str)
 
-f1 = "ice "
-f2 = "cream"
-print(f1 + f2)
+str = "I love Ice Cream."
+print(str, str.swapcase())
 
-# age = "ab12cd"
-age_str = input("Enter your age. ")
-print(age_str)
-if age_str.isdigit():
-    print("Age is valid.")
-else:
-    print("Age should be a number.")
-
-word = input("Enter a word. ")
-if word.isalpha():
-    print("The word is valid")
-else:
-    print("The word is not valid.")
+# f1 = "ice "
+# f2 = "cream"
+# print(f1 + f2)
+#
+# # age = "ab12cd"
+# age_str = input("Enter your age. ")
+# print(age_str)
+# if age_str.isdigit():
+#     print("Age is valid.")
+# else:
+#     print("Age should be a number.")
+#
+# word = input("Enter a word. ")
+# if word.isalpha():
+#     print("The word is valid")
+# else:
+#     print("The word is not valid.")
