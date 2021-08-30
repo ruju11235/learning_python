d925c0a1c4ed81741d372658d39747f74bfe8941
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-08-30 13:02:31 +0530

    exercise 5

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -6,8 +6,24 @@
 # - Print each letter in the sentence on a line.
 # - Count the number of occurrences of the alphabet in the sentence and print it.
 
-num_1 = input("Please enter a number: ")
-num_2 = input("Please enter another number: ")
-print("Your first number to the power your second number is")
+num_1 = int(input("Please enter a number: "))
+num_2 = int(input("Please enter another number: "))
+base_number = 1
+multiplication_count = 0
+while multiplication_count != num_2:
+    base_number = base_number * num_1
+    multiplication_count = multiplication_count + 1
+else:
+    print("num_1 to the power num_2 is", base_number)
+print(pow(num_1, num_2))
 
+print()
 sentence = input("Please enter a sentence: ")
+alphabet = input("Please enter a letter: ")
+letter_count = 0
+for w in sentence.lower():
+    print(w)
+    if w == alphabet:
+        letter_count = letter_count + 1
+print()
+print("Alphabet occurrences:", letter_count)
