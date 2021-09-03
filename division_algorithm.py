71db1cc09676fc4cf8d586ce98f2448938783a7b
Rujuta Mhaswade <rujuta.mhaswade@gmail.com>
2021-09-03 14:18:06 +0530

    corrected the division algorithm program, going to do multiplication tables next

--- division_algorithm.py
+++ division_algorithm.py
@@ -1,25 +1,28 @@
 dividend = int(input("Please enter a number: "))
 divisor = int(input("Please enter another number: "))
+if dividend == 0 or divisor == 0:
+    print("Please give a valid value next time!")
+    exit(0)
 quotient = dividend // divisor
 remainder = dividend % divisor
 
-# print()
+print()
 # The Division Algorithm using conditionals
-# if dividend < divisor:
-#     print(dividend, "is the answer.")
-# else:
-#     print("The answer to your question is", quotient)
-#     print("The remainder is", remainder)
-
-# print()
-# The Division Algorithm using a 'while' loop
-# while dividend >= divisor:
-#     print("The answer to your question is", quotient)
-#     print("The remainder is", remainder)
-#     exit(0)
-# else:
-#     print(dividend, "is the answer.")
+if dividend < divisor:
+    print("The quotient is 0.")
+    print(dividend, "is the remainder.")
+else:
+    print("The quotient is", quotient)
+    print("The remainder is", remainder)
 
 print()
-# The division Algorithm using a 'for' loop
-# for n in
+num_1 = dividend
+num_2 = divisor
+q = 0
+# The Division Algorithm using a 'while' loop
+while num_1 >= num_2:
+    num_1 = num_1 - num_2
+    q = q + 1
+else:
+    print("The quotient is", q)
+    print(num_1, "is the remainder.")
